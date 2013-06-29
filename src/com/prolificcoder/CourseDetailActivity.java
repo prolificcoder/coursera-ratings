55ad241b10c1aa6f284d4ad34181bc7fbd63b84e
Satyajit Malugu <satya@malugu.com>
2013-06-29 13:47:10 -0700

    Made the up and down buttons work

--- src/com/prolificcoder/CourseDetailActivity.java
+++ src/com/prolificcoder/CourseDetailActivity.java
@@ -3,43 +3,73 @@ package com.prolificcoder;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.parse.GetCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+
 public class CourseDetailActivity extends Activity {
 
-	public void onCreate(Bundle savedInstanceState){
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		this.setContentView(R.layout.single_list_item_view);
-		
+
 		Intent i = getIntent();
-		String name = i.getStringExtra("name");
+		final String courseName = i.getStringExtra("name");
 		int upvote = i.getIntExtra("upvote", 0);
 		int downvote = i.getIntExtra("downvote", 0);
 		String url = i.getStringExtra("url");
-		
-		TextView nameText = (TextView)findViewById(R.id.CourseName);
-		nameText.setText(name);
-		
-		TextView urlText = (TextView)findViewById(R.id.Url);
+
+		TextView nameText = (TextView) findViewById(R.id.CourseName);
+		nameText.setText(courseName);
+
+		TextView urlText = (TextView) findViewById(R.id.Url);
 		urlText.setText((url != null) ? url : "None specified");
-		
-		TextView ratingText = (TextView)findViewById(R.id.Rating);
+
+		TextView ratingText = (TextView) findViewById(R.id.Rating);
 		ratingText.setText(Helpers.average(upvote, downvote).toString());
-		
+
 		final Button buttonUp = (Button) findViewById(R.id.Up);
 		buttonUp.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                
-            }
-        });
-		
-        final Button buttonDown = (Button) findViewById(R.id.Down);
-        buttonDown.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                
-            }
-        });
+			public void onClick(View v) {
+				ParseQuery<ParseObject> query = ParseQuery.getQuery("courses");
+				query.whereEqualTo("name", courseName);
+				query.getFirstInBackground(new GetCallback<ParseObject>() {
+					  public void done(ParseObject object, ParseException e) {
+					    if (object == null) {
+					      Log.d("score", "The getFirst request failed.");
+					    } else {
+					      Log.d("score", "Retrieved the object.");
+					      object.increment("upvote");
+					      object.saveInBackground();
+					    }
+					  }
+				});
+			}
+		});
+
+		final Button buttonDown = (Button) findViewById(R.id.Down);
+		buttonDown.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				ParseQuery<ParseObject> query = ParseQuery.getQuery("courses");
+				query.whereEqualTo("name", courseName);
+				query.getFirstInBackground(new GetCallback<ParseObject>() {
+					  public void done(ParseObject object, ParseException e) {
+					    if (object == null) {
+					      Log.d("score", "The getFirst request failed.");
+					    } else {
+					      Log.d("score", "Retrieved the object.");
+					       object.increment("downvote");
+					       object.saveInBackground();
+					    }
+					  }
+				});
+			}
+		});
 	}
 }
