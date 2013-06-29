81656f1add2a6c4e4530400edb3abb727473ca9d
Satyajit Malugu <satya@malugu.com>
2013-06-29 13:07:54 -0700

    buttons

--- src/com/prolificcoder/CourseDetailActivity.java
+++ src/com/prolificcoder/CourseDetailActivity.java
@@ -1,19 +1,10 @@
 package com.prolificcoder;
 
-import java.util.List;
-
-import com.parse.ParseException;
-import com.parse.ParseObject;
-import com.parse.ParseQuery;
-
 import android.app.Activity;
-import android.app.Dialog;
-import android.app.ProgressDialog;
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ListView;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class CourseDetailActivity extends Activity {
@@ -37,5 +28,18 @@ public class CourseDetailActivity extends Activity {
 		TextView ratingText = (TextView)findViewById(R.id.Rating);
 		ratingText.setText(Helpers.average(upvote, downvote).toString());
 		
+		final Button buttonUp = (Button) findViewById(R.id.Up);
+		buttonUp.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                
+            }
+        });
+		
+        final Button buttonDown = (Button) findViewById(R.id.Down);
+        buttonDown.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                
+            }
+        });
 	}
 }
