7b6dcd9f60a6b89d4019457054e9a816ef04f7ed
Satyajit Malugu <satya@malugu.com>
2013-07-04 16:05:25 -0700

    saving categories, universities, querying course details from course list

---
+++ scripts/save_category.js
@@ -0,0 +1,41 @@
+var request = require('request');
+var Parse = require('parse').Parse;
+Parse.initialize("yCuUqabpEb7BHYRtits6iaOs740jpon1Eq6VhQCz", "Lm9XQz0xo1ORP0OvCHBZs8EFq6RJZJQwxMzqqTjA", "GPxPibULRJqKAZ0ik2LNG3nWBozfxUeS5C0Mtsw6");
+
+request({
+    uri: "https://www.coursera.org/maestro/api/category/list",
+    method: "GET",
+    json: true
+}, function (err, res, categories) {
+    for(var i=0; i<categories.length; i++) {
+        save_category(categories[i]);
+    }
+});
+
+function save_category(category) {
+    var categories = Parse.Object.extend("categories_te");
+    var local_category = new categories();
+    local_category.set("name", category.name);
+    local_category.set("short_name", category.short_name);
+
+    var query = new Parse.Query(categories);
+    query.equalTo("name", category.name);
+    query.count({
+        success: function (number) {
+            if (number == 0) {
+                local_category.save(null, {
+                    success: function (cour) {
+                        // Execute any logic that should take place after the object is saved.
+                        console.log('New object created with objectId: ' + cour.id);
+                    },
+                    error: function (category, error) {
+                        // Execute any logic that should take place if the save fails.
+                        // error is a Parse.Error with an error code and description.
+                        console.log('Failed to create new object, with error code: ' + error.description);
+                    }
+                });
+            }
+        }
+    })
+
+}
--- scripts/save_course_json.js
+++ scripts/save_course_json.js
@@ -1,46 +1,47 @@
 var request = require('request');
 var Parse = require('parse').Parse;
 Parse.initialize("yCuUqabpEb7BHYRtits6iaOs740jpon1Eq6VhQCz", "Lm9XQz0xo1ORP0OvCHBZs8EFq6RJZJQwxMzqqTjA", "GPxPibULRJqKAZ0ik2LNG3nWBozfxUeS5C0Mtsw6");
-//var client = new Client('https://www.coursera.org/maestro/api/topic/information?topic-id=compdata');
-
 request({
-    uri: "https://www.coursera.org/maestro/api/topic/information?topic-id=compdata",
+    uri: "https://www.coursera.org/maestro/api/topic/list",
     method: "GET",
     json: true
-}, function (err, res, course) {
-    console.log(course.about_the_course);
-    console.log(course.estimated_class_workload);
-    console.log(course.universities[0].name);
-    json_save(course);
+}, function (err, res, courses) {
+
+    for(var i=0; i<courses.length; i++) {
+        json_save(courses[i]);
+    }
 });
 
 function json_save(course) {
-    var Courses = Parse.Object.extend("courses_db");
-    var local_course = new Courses();
-    local_course.set("name", course.name);
-    local_course.set("Description", course.about_the_course);
-    local_course.set("work_load", course.estimated_class_workload);
+    request({
+            uri: "https://www.coursera.org/maestro/api/topic/information?topic-id=" + course.short_name,
+            method: "GET",
+            json: true
+        }, function (err, res, course) {
+        var Courses = Parse.Object.extend("courses_db");
+        var local_course = new Courses();
+        local_course.set("name", course.name);
+        local_course.set("Description", course.about_the_course);
+        local_course.set("work_load", course.estimated_class_workload);
 
-    var query = new Parse.Query(Courses);
-    query.equalTo("name", course.name);
-    query.count({
-        success: function (number) {
-            if (number == 0) {
-                local_course.save(null, {
-                    success: function (cour) {
-                        // Execute any logic that should take place after the object is saved.
-                        console.log('New object created with objectId: ' + cour.id);
-                        console.log('New object created with objectId: ' + cour.get("Description"));
-                        console.log('New object created with objectId: ' + cour.get("work_load"));
-                    },
-                    error: function (course, error) {
-                        // Execute any logic that should take place if the save fails.
-                        // error is a Parse.Error with an error code and description.
-                        console.log('Failed to create new object, with error code: ' + error.description);
-                    }
-                });
+        var query = new Parse.Query(Courses);
+        query.equalTo("name", course.name);
+        query.count({
+            success: function (number) {
+                if (number == 0) {
+                    local_course.save(null, {
+                        success: function (cour) {
+                            // Execute any logic that should take place after the object is saved.
+                            console.log('New object created with objectId: ' + cour.id);
+                        },
+                        error: function (course, error) {
+                            // Execute any logic that should take place if the save fails.
+                            // error is a Parse.Error with an error code and description.
+                            console.log('Failed to create new object, with error code: ' + error.description);
+                        }
+                    });
+                }
             }
-        }
-    })
-
+        });
+    });
 }
