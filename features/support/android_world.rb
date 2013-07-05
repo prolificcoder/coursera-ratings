af3f175f514594c02c2c0e20fc46dbc0ea9e1e77
Satyajit Malugu <satya@malugu.com>
2013-07-05 15:44:38 -0700

    Adding more pieces to run this on appium

---
+++ features/support/android_world.rb
@@ -0,0 +1,28 @@
+class AndroidWorld
+  def initialize
+    android_app = {
+        :app_path => ANDROID_PATH,
+        :app_package => 'com.prolificcoder',
+        :app_activity => '.CoursesListActivity',
+        :app_wait_activity => '.CoursesListActivity',
+        :app_name => ANDROID_APP_NAME
+    }
+    if(ENV['runon'] == 'sauce')
+      android_app[:app_path] = 'sauce-storage:' + ANDROID_APP_NAME
+      android_app[:sauce_username] = SAUCE_USERNAME
+      android_app[:sauce_access_key] = SAUCE_ACCESS_KEY
+    end
+    Appium::Driver.new(android_app).start_driver
+  end
+
+  def should_see(element_name)
+    s_text element_name
+  end
+
+  def platform_click(element_text)
+    (s_text element_text).click
+  end
+
+
+end
+
